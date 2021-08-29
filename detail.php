09f3655194c8ae447b3f0c4cd6a4b4d8404887ac
sonlv1905 <80377041+sonlv1905@users.noreply.github.com>
2021-08-29 08:42:33 +0700

    update detail

---
+++ detail.php
@@ -0,0 +1,215 @@
+<?php
+include "./layouts/header.php";
+?>
+
+<div class="main">
+        <div class="container__detail">
+            <div class="container__left">
+                <div class="news1">
+                    <h1 class="head-1">Hội nghị tập huấn trực tuyến phòng, chống dịch COVID-19 tại các cơ sở khám
+                        chữa bệnh</h1>
+                    <ul class="news1-history">
+                        <li class="news1-category"><a class="subtitle" href="#" title="Doanh nghiệp">doanh nghiệp</a></li>
+                        <li class="news1-time"><i class="far fa-clock"></i><span>04:10 PM 21/02/2020</span></li>
+                    </ul>
+                    <h3 class="news1-brief sapo2">
+                        Tại Hà Nội, Bộ TT&TT đã tổ chức Hội nghị giao ban quản lý nhà nước tháng 9 năm 2018. Ủy viên
+                        Trung ương Đảng, Bí thư Ban Cán sự Đảng, Quyền Bộ trưởng Bộ TT&TT Nguyễn Mạnh Hùng đã chủ trì
+                        Hội nghị.
+                    </h3>
+                    <ul class="facebook-item">
+                        <li>
+                            <a href="#" title="Thích">
+                                <i class="fas fa-thumbs-up"></i>
+                                <p>Thích</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" title="Chia sẻ">
+                                <i class="fab fa-facebook"></i>
+                                <p>Chia sẻ</p>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="news1-relevant">
+                        <li><i class="fas fa-circle"></i><a class="head-6" href="#"
+                                title="Hà Nội thúc đẩy quan hệ thương mại đầu tư với Nhật Bản trong điều kiện ">Hà Nội
+                                thúc đẩy quan hệ thương mại đầu tư với Nhật Bản trong điều kiện </a></li>
+                        <li><i class="fas fa-circle"></i><a class="head-6" href="#"
+                                title="Nhiều khả năng Triệu Quân Sự đã trốn thoát khỏi đèo Hải Vân">Nhiều khả năng Triệu
+                                Quân Sự đã trốn thoát khỏi đèo Hải Vân</a></li>
+                        <li><i class="fas fa-circle"></i><a class="head-6" href="#"
+                                title="Quảng Nam xây dựng chiến lược phát triển cho cây sâm Ngọc Linh ">Quảng Nam xây
+                                dựng chiến lược phát triển cho cây sâm Ngọc Linh </a></li>
+                    </ul>
+                    <div class="news1-list--content">
+                        <p class="content-item body">Hội nghị đã tập trung đánh giá những kết quả tích cực, những tồn tại của
+                            công tác quản lý nhà nước trong tháng 9/2018, đồng thời đề ra các giải pháp, mục tiêu cần
+                            thực hiện trong tháng 10/2018; chú trọng thúc đẩy phát triển các lĩnh vực trong ngành, gồm 6
+                            lĩnh vực: Bưu chính, Viễn thông, CNTT, An toàn thông tin, Công nghiệp ICT, Báo chí-
+                            PTTH-Xuất bản.</p>
+                        <p class="content-item body"> Tại Hội nghị, Quyền Bộ trưởng đã phân tích: Bộ phải xây dựng khung pháp
+                            lý cho từng lĩnh vực do Chính phủ giao hoặc nằm trong kế hoạch của Bộ. Các cơ quan, đơn vị
+                            của Bộ cần thường xuyên nắm bắt xem các đối tượng quản lý và người dân gặp khó khăn gì và
+                            phải giải quyết những khó khăn, vướng mắc này ra sao.
+                            Các văn bản pháp luật chính là công cụ để thúc đẩy các lĩnh vực quản lý nhà nước của Bộ phát
+                            triển lành mạnh, Quyền Bộ trưởng nhấn mạnh.</p>
+                        <img src="assets/img/image 4.png" alt="Vang">
+                        <p class="quote-content head-7">Ảnh minh họa. (Nguồn ĐSVN)</p>
+                        <p class="content-item body">Về mảng KHCN, tiêu chuẩn, công nghiệp CNTT, hiện Bộ Kế hoạch Đầu tư được
+                            Chính phủ giao xây dựng Chiến lược quốc gia 4.0. Quyền Bộ trưởng giao Vụ KHCN là đầu mối về
+                            4.0 và khẳng định Bộ TT&TT cần xây dựng phiên bản riêng về chiến lược 4.0 cho đất nước. Vụ
+                            KHCN cần tham khảo kinh nghiệm quốc tế về 4.0 và tham khảo ý kiến của các Sở TT&TT.</p>
+                    </div>
+                    <div class="news1-author head-6">Hoàng Anh</div>
+                    <div class="list-media">
+                        <ul class="facebook-item">
+                            <li>
+                                <a href="#" title="Thích">
+                                    <i class="fas fa-thumbs-up"></i>
+                                    <p>Thích</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" title="Chia sẻ">
+                                    <i class="fab fa-facebook"></i>
+                                    <p>Chia sẻ</p>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="media">
+                            <a href="#" title="Twitter">
+                                <li><i class="fab fa-twitter"></i></li>
+                            </a>
+                            <a href="#" title="Z">
+                                <li><img src="assets/img/z.png" alt="Z"></li>
+                            </a>
+                            <a href="#" title="Email">
+                                <li><i class="far fa-envelope"></i></li>
+                            </a>
+                        </ul>
+                    </div>
+                    <div class="news1-key"><strong class="head-7">Từ khóa: </strong><a class=" head-7" href="#" title="Gây tiếng ồn vượt ngưỡng">gây tiếng ồn vượt ngưỡng</a>, <a class=" head-7" href="#" title="Xả khói đen">xả khói đen</a>, <a class=" head-7" href="#" title="Di dời khỏi nội đô">di dời khỏi nội đô</a>
+                        ,<a class=" head-7" href="#" title="Mùi hóa chất phát tán"> mùi hóa chất phát tán</a></div>
+                </div>
+                <div class="container__lines">
+                    <div class="container__line-odd"></div>
+                    <div class="container__line-even"></div>
+                    <div class="container__line-odd"></div>
+                    <div class="container__line-even"></div>
+                    <div class="container__line-odd"></div>
+                </div>
+                <div class="news2">
+                    <h4 class="news2-title">
+                        <a class="heading-box" href="#" title="Tin cùng chuyên mục">TIN CÙNG CHUYÊN MỤC</a>
+                    </h4>
+                    <div class="news2-list">
+                        <!-- <div class="news2-list-item"> -->
+                            <?php 
+                                    $marketingnews = get_marketingNews();
+                                    foreach ($marketingnews as $item) {
+                                        $img = get_Image($item['id']);
+                                ?>
+                                <div class="news2-content">
+                                    <a href="#" title="<?php echo $item['title']; ?>"><img
+                                            src="assets/img/<?php echo $img['imgurl']; ?>" alt="anh"></a>
+                                    <h6><a class="head-6" href="#"
+                                            title="<?php echo $item['title']; ?>"
+                                            class="news2-title">
+                                            <?php echo $item['title']; ?>
+                                        </a></h6>
+                                </div>
+
+                            <?php } ?>
+                    </div>
+                </div>
+                <div class="line"></div>
+                <div class="news3">
+                    <h4 class="news3-category"><a class="heading-box" href="#" title="Tin nổi bật">Tin nổi bật</a></h4>
+                    <div class="news3-content">
+                        <div class="news3-left">
+                        <?php 
+                            $business = get_businessCate();
+                            $img = get_Image($business['id']);
+                        ?>
+                        <a href="detail.html"
+                            title='<?php echo $business['title']; ?>'><img
+                                src="assets/img/<?php echo $img['imgurl']; ?>" alt="DN"></a>
+                        <h2><a class="head-2" href="detail.html"
+                                title='<?php echo $business['title']; ?>'>
+                                <?php echo $business['title']; ?>
+                            </a></h2>
+                        <p class="sapo1">
+                            <?php echo $business['brief']; ?>
+                        </p>
+                        </div>
+                        <div class="news3-right">
+                        <?php
+                            $businessright = get_businessRight();
+                            for($i = 0 ; $i<3 ; $i++){
+                                if($i==0){
+                                    $img = get_Image($businessright[0]['id']);
+                        ?>
+                        <a href="#"
+                            title='<?php echo $businessright[0]['title']; ?>'><img
+                                src="assets/img/<?php echo $img['imgurl']; ?>" alt="DN"></a>
+                        <h4><a class="head-4" href="#"
+                                title='<?php echo $businessright[0]['title']; ?>'>
+                                <?php echo $businessright[0]['title']; ?>
+                            </a></h4>
+                                <?php }else if($i==1){ ?>
+                        <p class='first'><a href="#"
+                                title='<?php echo $businessright[1]['title']; ?>'>
+                                <?php echo $businessright[1]['title']; ?>
+                            </a></p>
+                                <?php }else{ ?>
+                        <p class='second'><a href="#"
+                                title='<?php echo $businessright[2]['title']; ?>'><?php echo $businessright[2]['title']; ?></a></p>
+                        <?php } } ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="line"></div>
+                <div class="news4">
+                    <h4 class="news4-category"><a class="heading-box" href="#" title="Đọc thêm">Đọc thêm</a></h4>
+                    <?php 
+                        $mainnews = get_mainNews();
+                        foreach($mainnews as $news){
+                            $img = get_Image($news['id']);
+                            $category = get_category($news['FK_category']);
+                    ?>
+                    <div class="news4-second--content">
+                        <a href="#" title="<?php echo $news['title']; ?>"><img
+                                src="assets/img/<?php echo $img['imgurl']; ?>" alt="anh"></a>
+                        <div class="second--contents">
+                            <h3>
+                                <a class="head-3" href="#"
+                                    title="<?php echo $news['title']; ?>">
+                                    <?php echo $news['title']; ?>
+                                </a>
+                            </h3>
+                            <div class="second-category "><a class="subtitle" href="#" title='<?php echo $category['title']; ?>'><?php echo $category['title']; ?></a></div>
+                            <div class="second-content sapo1">
+                                <?php echo $news['brief']; ?>
+                            </div>
+                        </div>
+                    </div>
+                    <?php } ?>
+                    <div class="news4-btn">
+                        <a class="head-7" href="#" title="Xem thêm">
+                            <p>Xem thêm</p>
+                            <img src="assets/img/Vector 40 (Stroke).png" alt="anh">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="container__right">
+
+            </div>
+        </div>
+    </div>
+
+
+<?php
+        include './layouts/footer.php';
+    ?>
